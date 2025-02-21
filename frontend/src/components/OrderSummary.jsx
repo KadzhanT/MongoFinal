0a4cd25f8ab28bbab71e201708615f7a71c0b81b
Nurken <nurken.serdakov.2000@mail.ru>
2025-02-21 22:20:35 +0500

    stripe

--- frontend/src/components/OrderSummary.jsx
+++ frontend/src/components/OrderSummary.jsx
@@ -6,7 +6,7 @@ import { loadStripe } from "@stripe/stripe-js";
 import axios from "../lib/axios";
 
 const stripePromise = loadStripe(
-	"pk_test_51KZYccCoOZF2UhtOwdXQl3vcizup20zqKqT9hVUIsVzsdBrhqbUI2fE0ZdEVLdZfeHjeyFXtqaNsyCJCmZWnjNZa00PzMAjlcL"
+	"pk_test_51Quzo7IWrGmkEb5MtviVdwRkeQTgYRkiYupvr7JOnZbu48mSamO8gL14GeVXbrNKm8EHve40iSYFtEDlRcajMYL600GksEDt83"
 );
 
 const OrderSummary = () => {
